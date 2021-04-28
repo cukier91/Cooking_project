fe3d70ae91926d6851c222ec36e58d7d57acdbda
cukier91 <konarowski.bartosz@gmail.com>
2021-04-28 10:35:44 +0200

    All models created

--- cooking/models.py
+++ cooking/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class IngredientsModel(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeModel(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    date = models.DateTimeField(default=datetime.now(), blank=True)
+    ingredients = models.ManyToManyField(IngredientsModel, through='RecipeIngredientsModel')
+
+
+class RecipeIngredientsModel(models.Model):
+    ingredients = models.ForeignKey(IngredientsModel, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(RecipeModel, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+
+
+
+
+
