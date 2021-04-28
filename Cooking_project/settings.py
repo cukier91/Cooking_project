a42ad3eca875fe7d67dac039a1492700a81a116d
cukier91 <konarowski.bartosz@gmail.com>
2021-04-28 09:56:07 +0200

    zmianna settings db

--- Cooking_project/settings.py
+++ Cooking_project/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Cooking_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'cooking_db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': '127.0.0.1',
     }
 }
 
