736a169e85589ce56fe7e3fb9cac0692dd23e568
cukier91 <konarowski.bartosz@gmail.com>
2021-08-23 21:33:17 +0200

    Wyświetla tylko te produkty które mają być przeliczane

--- cooking/static/js/script.js
+++ cooking/static/js/script.js
@@ -22,34 +22,51 @@ const dict = document.getElementById('dictionary')
 const check = document.getElementById('check')
 const products = document.getElementById('products')
 
-function catchList(){
-    fetch('http://127.0.0.1:8000/api/calories').then(async response => {
-        return response.json();
-    }).then(data => {
-        products.innerHTML = data.map(product => {
-            return `
-            <tr>
-            <td>${product.name}</td>
-            <td>${product.energy}</td>
-            <td>${product.fat}</td>
-            <td>${product.carbohydrates}</td>
-            <td>${product.fiber}</td>
-            <td>${product.protein}</td>
-            <td>${product.salt}</td>
-            <td>${product.sugar}</td>
-            </tr>
-            `;
-        }).join("")
-    })
-
-
-}
-
-check.addEventListener('click',catchList)
-
 button.addEventListener('click', function (e){
     let clone = item.cloneNode(true);
     document.getElementById('section_test').appendChild(clone)
 
 })
+
+const productsArray = []
+function catchList(){
+
+    const quantityProduct = document.querySelectorAll('#selector')
+    quantityProduct.forEach(element =>{
+        productsArray.push(element.value)
+        console.log(productsArray)
+    })
+    fetch('http://127.0.0.1:8000/api/calories').then(async response => {
+        return response.json();
+    }).then(data => {
+        products.innerHTML = data.map(product => {
+                return `
+                <tr>
+                <td class="product_name">${product.name}</td>
+                <td>${product.energy}</td>
+                <td>${product.fat}</td>
+                <td>${product.carbohydrates}</td>
+                <td>${product.fiber}</td>
+                <td>${product.protein}</td>
+                <td>${product.salt}</td>
+                <td>${product.sugar}</td>
+                </tr>
+                `;
+            }).join("")
+        const pass = document.querySelectorAll("td.product_name")
+    pass.forEach(element =>{
+      if(productsArray.includes(`${element.innerHTML}`) !== true ){
+          element.parentElement.hidden = true
+      }
+    })
+
+    })
+
+
+}
+check.addEventListener('click',catchList)
+
+
+
+
 
